33ce0c34812d8b4ad7d57d278f3ea839fa6ac7bb
davidf <davidf@alleninstitute.org>
2015-08-20 09:43:51 -0700

    tweaking network examples

--- other_examples/multicell/input/convert_to_vtk.py
+++ other_examples/multicell/input/convert_to_vtk.py
@@ -6,13 +6,37 @@ import scipy.sparse
 from collections import Counter
 import argparse
 
-def write_vtk(file_name, cells, connections):
+SKIP_FIELDS = ['x', 'y', 'z', 'index', 'gid']
+
+def get_vtk_dtype(obj):
+    try:
+        int(obj)
+        return 'int'
+    except ValueError:
+        try:
+            float(obj)
+            return 'float'
+        except ValueError:
+            return 'string'
+
+def convert_string_types_to_int(cells, field):
     index = 0
-    cell_types = {}
+    values = {}
     for cell in cells:
-        if cell['type'] not in cell_types:
-            cell_types[cell['type']] = index
+        if cell[field] not in values:
+            values[cell[field]] = index
             index += 1
+
+    for cell in cells:
+        cell[field] = values[cell[field]]
+
+def write_vtk(file_name, cells, connections):
+    scalar_fields = [ ( k, get_vtk_dtype(v) ) for k,v in cells[0].iteritems() if k not in SKIP_FIELDS ]
+
+    for i, (field, dtype) in enumerate(scalar_fields):
+        if dtype == 'string':
+            convert_string_types_to_int(cells, field)
+            scalar_fields[i] = (field, 'int')
 
     m = connections.tocoo()
 
@@ -24,11 +48,7 @@ def write_vtk(file_name, cells, connections):
         
         f.write("POINTS %d float\n" % len(cells))
         for cell in cells:
-            f.write("%f %f %f\n" % ( cell['x'], cell['y'], cell['z'] ))
-
-        print m.row
-        print m.col
-        print m.data
+            f.write("%s %s %s\n" % ( cell['x'], cell['y'], cell['z'] ))
 
         f.write("LINES %d %d\n" % (len(m.row), len(m.row)*3))
         for i, j in zip(m.row, m.col):
@@ -36,30 +56,18 @@ def write_vtk(file_name, cells, connections):
 
         f.write("\n")
 
-        f.write("POINT_DATA %d\n" % len(cells))
-        f.write("SCALARS angle float 1\n")
-        f.write("LOOKUP_TABLE default\n")
+        if len(scalar_fields):
+            f.write("POINT_DATA %d\n" % len(cells))
 
-        for cell in cells:
-            f.write("%f\n" % cell['angle'])
+            for field, dtype in scalar_fields:
+                f.write("SCALARS %s %s 1\n" % (field, dtype))
+                f.write("LOOKUP_TABLE default\n")
 
-        f.write("SCALARS cell_type int 1\n")
-        f.write("LOOKUP_TABLE default\n")
-        for cell in cells:
-            f.write("%d\n" % cell_types[cell['type']])
+                for cell in cells:
+                    f.write("%s\n" % cell[field])
 
+                f.write("\n")
 
-        f.write("\n")
-
-        f.write("CELL_DATA %d\n" % len(m.data))
-        f.write("SCALARS weight float 1\n")
-        f.write("LOOKUP_TABLE default\n")
-
-        for d in m.data:
-            f.write("%f\n" % d)
-
-        f.write("\n")
-            
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('cells_csv', help='CSV containing cell metadata')
@@ -69,7 +77,9 @@ def main():
     args = parser.parse_args()
 
     # read in the cell CSV
-    cells = pd.DataFrame.from_csv(args.cells_csv).to_dict(orient='records')
+    with open(args.cells_csv, 'r') as f:
+        r = csv.DictReader(f)
+        cells = list(r)
 
     # read in the connections from the H5 file
     h5u = Hdf5Util()
